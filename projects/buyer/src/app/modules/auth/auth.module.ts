8777954ddba2d6723e5765d897243fdf886d9c3d
Abdalrhman <bodygrapic@gmail.com>
2023-06-02 12:06:10 +0300

    Add (email, security-code)  Components in auth for buyer
    - Add email Component in 'modules/auth/email' for can user search him email if forgotten for buyer
    
    - Add security-code Component in 'modules/auth/security-code' for check the code is truly for buyer

--- projects/buyer/src/app/modules/auth/auth.module.ts
+++ projects/buyer/src/app/modules/auth/auth.module.ts
@@ -5,13 +5,17 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
+import { SecurityCodeComponent } from './security-code/security-code.component';
 
 
 @NgModule({
   declarations: [
     AuthComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    EmailComponent,
+    SecurityCodeComponent
   ],
   imports: [
     CommonModule,
---
+++ projects/buyer/src/app/modules/auth/security-code/security-code.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-code',
+  templateUrl: './security-code.component.html',
+  styleUrls: ['./security-code.component.scss']
+})
+export class SecurityCodeComponent {
+
+}

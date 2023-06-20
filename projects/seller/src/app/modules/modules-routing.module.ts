f2b4829c339133871b476e35263a0af51547060d
Abdalrhman <bodygrapic@gmail.com>
2023-06-21 00:37:27 +0300

    Edit Main Routing For Seller
    Create Modules Component for Seller
    
    Edit in App Routing
    
    Edit in Modules Routing

--- projects/seller/src/app/modules/modules-routing.module.ts
+++ projects/seller/src/app/modules/modules-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModulesComponent } from './modules.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ModulesComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+      },
+      {
+        path: 'auth',
+        loadChildren: () =>
+          import('./auth/auth.module').then((m) => m.AuthModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ModulesRoutingModule { }
+export class ModulesRoutingModule {}

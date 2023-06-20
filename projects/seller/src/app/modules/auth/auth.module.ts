86b616aa1db62020e1beecabcf86146fdffbf85f
Abdalrhman <bodygrapic@gmail.com>
2023-06-21 00:22:24 +0300

    Create Auth Components For Seller

--- projects/seller/src/app/modules/auth/auth.module.ts
+++ projects/seller/src/app/modules/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
+import { SecurityCodeComponent } from './security-code/security-code.component';
+import { AuthComponent } from './auth.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SigninComponent,
+    SignupComponent,
+    EmailComponent,
+    SecurityCodeComponent,
+    AuthComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule

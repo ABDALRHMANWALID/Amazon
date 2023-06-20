0bfce664183af095d0897d67268c72951c2419d0
Abdalrhman <bodygrapic@gmail.com>
2023-06-21 00:52:38 +0300

    Edit Dashboard Routing For Seller
    Create Dashboard Component for Seller

--- projects/seller/src/app/modules/dashboard/dashboard-routing.module.ts
+++ projects/seller/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,60 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'advertisements',
+        loadChildren: () =>
+          import('./advertisements/advertisements.module').then(
+            (m) => m.AdvertisementsModule
+          ),
+      },
+      {
+        path: 'chat',
+        loadChildren: () =>
+          import('./chat/chat.module').then((m) => m.ChatModule),
+      },
+      {
+        path: 'deals',
+        loadChildren: () =>
+          import('./deals/deals.module').then((m) => m.DealsModule),
+      },
+      {
+        path: 'orders',
+        loadChildren: () =>
+          import('./orders/orders.module').then((m) => m.OrdersModule),
+      },
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./products/products.module').then((m) => m.ProductsModule),
+      },
+      {
+        path: 'me',
+        loadChildren: () =>
+          import('./profile/profile.module').then((m) => m.ProfileModule),
+      },
+      {
+        path: 'help',
+        loadChildren: () =>
+          import('./support/support.module').then((m) => m.SupportModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}

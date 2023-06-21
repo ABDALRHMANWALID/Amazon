fb3c32a80692b38aa43288128f6ae552ccfae1df
Abdalrhman <bodygrapic@gmail.com>
2023-06-21 16:32:53 +0300

    Add Components & Routing Products For Seller

--- projects/seller/src/app/modules/dashboard/products/products-routing.module.ts
+++ projects/seller/src/app/modules/dashboard/products/products-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AllProductsComponent },
+  { path: 'add', component: AddProductsComponent },
+  { path: ':id', component: DetailsProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
--- projects/seller/src/app/modules/dashboard/products/products.module.ts
+++ projects/seller/src/app/modules/dashboard/products/products.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent,
+    AddProductsComponent,
+    AllProductsComponent,
+    DetailsProductComponent,
+    DeleteProductComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule

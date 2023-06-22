883382e43e700df61aa9c7730eac7dd2daac8b5b
Abdalrhman <bodygrapic@gmail.com>
2023-06-22 15:08:09 +0300

    Edit Material Module for Admin

--- projects/admin/src/app/modules/modules.module.ts
+++ projects/admin/src/app/modules/modules.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ModulesRoutingModule } from './modules-routing.module';
 import { ModulesComponent } from './modules.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ModulesComponent
-  ],
-  imports: [
-    CommonModule,
-    ModulesRoutingModule
-  ]
+  declarations: [ModulesComponent],
+  imports: [CommonModule, ModulesRoutingModule, SharedModule],
 })
-export class ModulesModule { }
+export class ModulesModule {}

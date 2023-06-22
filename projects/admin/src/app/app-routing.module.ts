93d729c5bbef9d1bc08c927a0ce74b2254628f39
Abdalrhman <bodygrapic@gmail.com>
2023-06-22 15:14:13 +0300

    Edit Frist Main Routing for Admin

--- projects/admin/src/app/app-routing.module.ts
+++ projects/admin/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/modules.module').then((m) => m.ModulesModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

48cee3ff846d0cb5e6f17800e24a232821ac9681
Abdalrhman <bodygrapic@gmail.com>
2023-07-02 20:09:53 +0300

    Add Routing to orders/new-order For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/new-order/new-order-routing.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/new-order/new-order-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewOrderComponent } from './new-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NewOrderComponent,
+    children: [
+      {
+        path: 'address',
+        loadChildren: () =>
+          import('./address/address.module').then((m) => m.AddressModule),
+      },
+      {
+        path: 'info-order',
+        loadChildren: () =>
+          import('./info-order/info-order.module').then(
+            (m) => m.InfoOrderModule
+          ),
+      },
+      {
+        path: 'payment',
+        loadChildren: () =>
+          import('./payment/payment.module').then((m) => m.PaymentModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class NewOrderRoutingModule { }
+export class NewOrderRoutingModule {}

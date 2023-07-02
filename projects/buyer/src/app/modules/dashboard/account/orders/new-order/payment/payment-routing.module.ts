0031b1b88d1bdd6139cde74e27dd9dc414bdd146
Abdalrhman <bodygrapic@gmail.com>
2023-07-02 20:19:21 +0300

    Add Routing to orders/new-order/payment For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/new-order/payment/payment-routing.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/new-order/payment/payment-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentComponent } from './payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PaymentComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PaymentRoutingModule { }
+export class PaymentRoutingModule {}

51e3cdbe640b77da6f8b5aac4a09743d02af3952
Abdalrhman <bodygrapic@gmail.com>
2023-07-02 20:14:20 +0300

    Add Routing to orders/new-order/address For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/new-order/address/address-routing.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/new-order/address/address-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressComponent } from './address.component';
+import { OptionsAddressComponent } from './options-address/options-address.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { AddressMapComponent } from './address-map/address-map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AddressComponent,
+    children: [
+      { path: '', component: OptionsAddressComponent },
+      {
+        path: 'form',
+        component: AddressFormComponent,
+      },
+      {
+        path: 'map',
+        component: AddressMapComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AddressRoutingModule { }
+export class AddressRoutingModule {}

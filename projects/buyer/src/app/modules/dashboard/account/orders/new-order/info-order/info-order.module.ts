dd6beb46c61d730b16e86c77c0baf48173b436e8
Abdalrhman <bodygrapic@gmail.com>
2023-07-02 18:52:58 +0300

    Add orders/new-order/info-order Module For Buyer

---
+++ projects/buyer/src/app/modules/dashboard/account/orders/new-order/info-order/info-order.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InfoOrderRoutingModule } from './info-order-routing.module';
+import { InfoOrderComponent } from './info-order.component';
+
+
+@NgModule({
+  declarations: [
+    InfoOrderComponent
+  ],
+  imports: [
+    CommonModule,
+    InfoOrderRoutingModule
+  ]
+})
+export class InfoOrderModule { }

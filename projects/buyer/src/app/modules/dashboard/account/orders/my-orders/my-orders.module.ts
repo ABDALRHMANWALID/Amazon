479bb949bea3998b9efbf4ff66452ab0cb8f1d6f
Abdalrhman <bodygrapic@gmail.com>
2023-07-02 19:39:10 +0300

    Add Components orders/my-orders For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MyOrdersRoutingModule } from './my-orders-routing.module';
 import { MyOrdersComponent } from './my-orders.component';
+import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { DetailsOrderComponent } from './details-order/details-order.component';
 
 
 @NgModule({
   declarations: [
-    MyOrdersComponent
+    MyOrdersComponent,
+    AllOrdersComponent,
+    DetailsOrderComponent
   ],
   imports: [
     CommonModule,

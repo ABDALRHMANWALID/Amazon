c06b8edfa9d0692b81173af3fffc491f75d81042
Abdalrhman <bodygrapic@gmail.com>
2023-07-05 15:53:58 +0300

    Add UI orders/my-orders/all-orders For Buyer
    Import Material Module in orders/my-orders For Buyer
    
    Add Style to orders/my-orders For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders.module.ts
@@ -5,17 +5,10 @@ import { MyOrdersRoutingModule } from './my-orders-routing.module';
 import { MyOrdersComponent } from './my-orders.component';
 import { AllOrdersComponent } from './all-orders/all-orders.component';
 import { DetailsOrderComponent } from './details-order/details-order.component';
-
+import { MaterialModule } from 'projects/buyer/src/app/shared/material/material.module';
 
 @NgModule({
-  declarations: [
-    MyOrdersComponent,
-    AllOrdersComponent,
-    DetailsOrderComponent
-  ],
-  imports: [
-    CommonModule,
-    MyOrdersRoutingModule
-  ]
+  declarations: [MyOrdersComponent, AllOrdersComponent, DetailsOrderComponent],
+  imports: [CommonModule, MyOrdersRoutingModule, MaterialModule],
 })
-export class MyOrdersModule { }
+export class MyOrdersModule {}

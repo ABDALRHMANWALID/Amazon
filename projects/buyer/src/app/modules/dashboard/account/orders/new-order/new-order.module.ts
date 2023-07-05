0dd5b2070415c2da30144dc3c066ea9772ed3ed6
Abdalrhman <bodygrapic@gmail.com>
2023-07-05 15:58:08 +0300

    Add UI orders/new-order For Buyer
    Import Material Module in orders/new-order For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/new-order/new-order.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/new-order/new-order.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { NewOrderRoutingModule } from './new-order-routing.module';
 import { NewOrderComponent } from './new-order.component';
-
+import { MaterialModule } from 'projects/buyer/src/app/shared/material/material.module';
+import { AddressComponent } from './address/address.component';
+import { InfoOrderComponent } from './info-order/info-order.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    NewOrderComponent
+    NewOrderComponent,
+    AddressComponent,
+    InfoOrderComponent,
+    PaymentComponent,
   ],
-  imports: [
-    CommonModule,
-    NewOrderRoutingModule
-  ]
+  imports: [CommonModule, NewOrderRoutingModule, MaterialModule],
 })
-export class NewOrderModule { }
+export class NewOrderModule {}

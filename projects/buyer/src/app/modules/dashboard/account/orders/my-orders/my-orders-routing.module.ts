25dff49ee9c02834346911a7a2b1a8d92b982c3e
Abdalrhman <bodygrapic@gmail.com>
2023-07-05 15:48:48 +0300

    Add Routing to orders/my-order For Buyer

--- projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders-routing.module.ts
+++ projects/buyer/src/app/modules/dashboard/account/orders/my-orders/my-orders-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { DetailsOrderComponent } from './details-order/details-order.component';
+import { MyOrdersComponent } from './my-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MyOrdersComponent,
+    children: [
+      {
+        path: '',
+        component: AllOrdersComponent,
+      },
+      {
+        path: ':id',
+        component: DetailsOrderComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MyOrdersRoutingModule { }
+export class MyOrdersRoutingModule {}

886c32bb4295b6c336eb14ea6c4b464f1f2544bf
Abdalrhman <bodygrapic@gmail.com>
2023-07-06 01:29:27 +0300

    Add UI seller/products-seller For Buyer

--- projects/buyer/src/app/modules/dashboard/seller/products-seller/products-seller.component.ts
+++ projects/buyer/src/app/modules/dashboard/seller/products-seller/products-seller.component.ts
@@ -3,13 +3,67 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products-seller',
   templateUrl: './products-seller.component.html',
-  styleUrls: ['./products-seller.component.scss']
+  styleUrls: ['./products-seller.component.scss'],
 })
 export class ProductsSellerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  products: any[] = [
+    {
+      id: 1,
+      name: 'Oppo A96 Dual SIM',
+      price: 9188,
+      lastPrice: 10000,
+      image: 'assets/images/prod-1.jpg',
+      discount: 99,
+      rating: [1, 1, 1, 1, 1],
+      inCart: true,
+      inFavourites: false,
+    },
+    {
+      id: 2,
+      name: 'Oppo A96 Dual SIM',
+      price: 9188,
+      lastPrice: 10000,
+      image: 'assets/images/prod-1.jpg',
+      discount: 99,
+      rating: [1, 1, 0.5, 0, 0],
+      inCart: false,
+      inFavourites: true,
+    },
+    {
+      id: 3,
+      name: 'Oppo A96 Dual SIM',
+      price: 9188,
+      lastPrice: 10000,
+      image: 'assets/images/prod-1.jpg',
+      discount: 99,
+      rating: [1, 1, 1, 1, 1],
+      inCart: true,
+      inFavourites: false,
+    },
+    {
+      id: 4,
+      name: 'Oppo A96 Dual SIM',
+      price: 9188,
+      lastPrice: 10000,
+      image: 'assets/images/prod-1.jpg',
+      discount: 99,
+      rating: [1, 1, 0.5, 0, 0],
+      inCart: false,
+      inFavourites: true,
+    },
+    {
+      id: 5,
+      name: 'Oppo A96 Dual SIM',
+      price: 9188,
+      lastPrice: 10000,
+      image: 'assets/images/prod-1.jpg',
+      discount: 99,
+      rating: [1, 1, 1, 1, 1],
+      inCart: true,
+      inFavourites: false,
+    },
+  ];
 }
